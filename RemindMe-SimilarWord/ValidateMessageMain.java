290cada883970c042dca57e95d4ebf4da0848931
AbhilashaSingh <miss.abhilashasingh@gmail.com>
2014-06-05 22:18:58 -0400

    Updated ValidateMessageMain.java

--- RemindMe-SimilarWord/ValidateMessageMain.java
+++ RemindMe-SimilarWord/ValidateMessageMain.java
@@ -11,7 +11,6 @@ public class ValidateMessageMain {
 		String message = "Namaste! Your bill dated 12-Apr-14 of Rs 1589 for your airtel fixedline 08042132154 is due on 01-May-14. ignore if paid.";
 		ValidateMessage val = new ValidateMessage();
 		boolean isValidVal = val.isValid(message);
-		System.out.println(isValidVal);
 	}
 
 }
